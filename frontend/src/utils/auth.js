18d3a7aa99ce423c8a66eb5efd5bfa65d4d19f69
prateek-1110 <b23bb1033@iitj.ac.in>
2026-03-15 02:23:39 +0530

    adding console.log

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -6,6 +6,8 @@ export const isLoggedIn = () => {
 export const loginWithGoogle = () => {
   const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
   window.location.href = `${API_URL}/accounts/google/login/`;
+    console.log("Button clicked! Trying to redirect to:", `${API_URL}/accounts/google/login/`);
+
 };
 
 export const logout = () => {
